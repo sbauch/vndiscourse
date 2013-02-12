1a2e536df5e4d8b78766aa74c01a0aa881c2ad08
Sam Bauch <sbauch@gmail.com>
2013-02-12 14:18:46 -0500

    more mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,8 @@ Discourse::Application.configure do
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
 
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.sendmail_settings = {arguments: '-i'}
+  # config.action_mailer.delivery_method = :sendmail
+  #  config.action_mailer.sendmail_settings = {arguments: '-i'}
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
