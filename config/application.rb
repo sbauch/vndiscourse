efa3007433c42943adce2faa8370243c3099dc85
Sam Bauch <sbauch@gmail.com>
2013-02-21 21:03:48 -0500

    heroku

--- config/application.rb
+++ config/application.rb
@@ -83,7 +83,7 @@ module Discourse
     config.handlebars.templates_root = 'discourse/templates'
 
     # Use redis for our cache
-    redis_config = YAML::load(File.open("./redis.yml"))[Rails.env]
+    redis_config = YAML::load(File.open("redis.yml"))[Rails.env]
     if Rails.env == "production"
 			redis_store = ActiveSupport::Cache::RedisStore.new "redis://redistogo:#{redis_config["password"]}@#{redis_config["host"]}:#{redis_config["port"]}"
 		else
