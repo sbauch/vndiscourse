872f32c6264b46d3513ad93ff8c9fd58d7dce59f
Sam Bauch <sbauch@gmail.com>
2013-03-11 14:54:13 -0400

    wtf redis

--- lib/discourse_redis.rb
+++ lib/discourse_redis.rb
@@ -2,21 +2,17 @@
 #  A wrapper around redis that namespaces keys with the current site id
 #
 class DiscourseRedis
-  
+
   def initialize
-    @config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails.env]
-    if Rails.env == 'production'
-      redis_opts = {:host => @config['host'], :port => @config['port'], :password => @config['password']}
-    else
-      redis_opts = {:host => @config['host'], :port => @config['port'], :db => @config['db']}
-    end
-    @redis = Redis.new(redis_opts)    
+    @config = URI.parse(ENV['REDISTOGO_URL'])
+    redis_opts = {:host => @config.host, :port => @config.port, :db => 0, :username => @config.user, :password => @config.password}
+    @redis = Redis.new(redis_opts)
   end
 
   # prefix the key with the namespace
   def method_missing(meth, *args, &block)
     if @redis.respond_to?(meth)
-      @redis.send(meth, *args, &block)  
+      @redis.send(meth, *args, &block)
     else
       super
     end
@@ -39,26 +35,8 @@ class DiscourseRedis
     RailsMultisite::ConnectionManagement.current_db
   end
 
-  def url 
-    # if Rails.env == 'production' 
-      "redis://redistogo:#{@config['password']}@#{@config['host']}:#{@config['port']}"
-    # else
-    #    "redis://#{@config['host']}:#{@config['port']}/#{@config['db']}"
-    #  end
-  end 
-
-  def self.new_redis_store
-    # redis_config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails.env]
-    #    
-    #    if Rails.env == 'production' 
-      redis_store = ActiveSupport::Cache::RedisStore.new "redis://redistogo:#{@config['password']}@#{@config['host']}:#{@config['port']}"
-    # else
-    #   redis_store = ActiveSupport::Cache::RedisStore.new "redis://#{@config['host']}:#{@config['port']}/#{@config['db']}"
-    # end
-    # "redis://#{redis_config['host']}:#{redis_config['port']}/#{redis_config['cache_db']}"
-    # redis_store.options[:namespace] = -> { DiscourseRedis.namespace }
-    # redis_store
+  def url
+    "#{ENV['REDISTOGO_URL']}0"
   end
-
 
 end
