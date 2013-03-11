8d637f1333ec50d70531f86b35028a60881fc923
Sam Bauch <sbauch@gmail.com>
2013-03-11 16:48:54 -0400

    connection mgmt

--- vendor/gems/rails_multisite/lib/rails_multisite/connection_management.rb
+++ vendor/gems/rails_multisite/lib/rails_multisite/connection_management.rb
@@ -55,13 +55,10 @@ module RailsMultisite
       @@config_filename ||= File.absolute_path(Rails.root.to_s + "/" + RailsMultisite::ConnectionManagement::CONFIG_FILE)
     end
 
-   def self.current_hostname 
-    # if Rails.env == 'production' 
-      ActiveRecord::Base.connection_pool.spec.config[:host]
-    # else
-    #      ActiveRecord::Base.connection_pool.spec.config[:host_names].first
-    #    end
-  end 
+    def self.current_hostname
+      config = ActiveRecord::Base.connection_pool.spec.config
+      config[:host_names].nil? ? config[:host] : config[:host_names].first
+    end
   
   def self.clear_settings!
       @@db_spec_cache = nil
