829a7b9d76519c92d60e21f40baac03086054f6b
Sam Bauch <sbauch@gmail.com>
2013-03-20 12:00:50 -0400

    wtf mang

--- vendor/gems/discourse_user_directory/app/controllers/directories_controller.rb
+++ vendor/gems/discourse_user_directory/app/controllers/directories_controller.rb
@@ -3,7 +3,7 @@ class DirectoriesController < ApplicationController
     if params[:filter].present?   
       params[:filter].split(' ').each do |filter|
         @users = User.order("COALESCE(last_seen_at, to_date('1970-01-01', 'YYYY-MM-DD')) DESC, username") unless !@users.nil?
-        @users = @users.where('username_lower ilike :filter or name ilike :filter or teams ilike :filter or position ilike :filter', filter: "%#{filter}%")   
+        @users = @users.where('username_lower ilike :filter or name ilike :filter or teams ilike :filter or position ilike :filter or short_position ilike :filter', filter: "%#{filter}%")   
       end
     else
       @users = User.order("COALESCE(last_seen_at, to_date('1970-01-01', 'YYYY-MM-DD')) DESC, username").limit(20)
