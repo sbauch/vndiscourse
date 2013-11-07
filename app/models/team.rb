533d87c8b955972a4284e01d8ed8c65d5ba11d3f
Sam Bauch <sbauch@gmail.com>
2013-11-07 10:34:57 -0500

    after delete hook on team

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,16 @@
 class Team < ActiveRecord::Base
   attr_accessible :name
+  
+  before_destroy :remove_from_user_teams
+  
+  
+  def remove_from_user_teams
+    User.all.each do |u|
+      u.teams_hash = u.teams_hash.delete_if{|id,name| id == self.id}
+      u.teams = u.teams_hash.collect{|k,v| v }.to_sentence
+      u.save
+    end    
+  end  
+  
+  
 end
